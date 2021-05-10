be686bca245513b04a053852829d596830022b08
edelmutic <edelmutic@gmail.com>
2021-05-10 12:08:47 +0300

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello');
