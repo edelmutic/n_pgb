4d46614f29f8dbb1abc398102089be57475d5a64
edelmutic <edelmutic@gmail.com>
2021-05-11 12:19:15 +0300

    little updates

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,9 @@
 class UserController {
-  async createUser(req, res) {}
+  async createUser(req, res) {
+    const { name, surname } = req.body;
+    console.log(name, surname);
+    res.json('ok');
+  }
   async getUsers(req, res) {}
   async getOneUser(req, res) {}
   async updateUser(req, res) {}
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
 app.use('/api', router);
 
 app.listen(PORT, () => {
