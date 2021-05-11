41bf91410b284110b62a281435834cab91ff4e3c
edelmutic <edelmutic@gmail.com>
2021-05-11 12:06:15 +0300

    update basic server

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const router = require('./routes/user.routes');
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening at ${PORT}`);
