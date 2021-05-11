9317ec74ee46c1bdfcb7613effc418db51385aee
edelmutic <edelmutic@gmail.com>
2021-05-11 12:45:31 +0300

    little updates and db init

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,8 +1,13 @@
+const db = require('../db');
+
 class UserController {
   async createUser(req, res) {
     const { name, surname } = req.body;
-    console.log(name, surname);
-    res.json('ok');
+    const newPerson = await db.query(
+      'INSERT INTO person (name, surname) values ($1, $2) RETURNING *',
+      [name, surname]
+    );
+    res.json(newPerson);
   }
   async getUsers(req, res) {}
   async getOneUser(req, res) {}
