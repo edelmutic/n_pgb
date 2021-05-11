ade938e5a61fb7184394809286bca5f84481a784
edelmutic <edelmutic@gmail.com>
2021-05-11 14:30:46 +0300

    update post controller

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,6 +1,17 @@
 class PostController {
-  async createPost(req, res) {}
-  async getPostByUser(req, res) {}
+  async createPost(req, res) {
+    const { title, content, userId } = req.body;
+    const post = await db.query(
+      'INSERT INTO post (title, content, userId) values ($1, $2, $3) RETURNING *',
+      [title, content, userId]
+    );
+    res.json(post.rows[0]);
+  }
+  async getPostByUser(req, res) {
+    const id = req.query.id;
+    const posts = await db.query('SELECT * FROM post where user_id = $1', [id]);
+    res.json(posts.rows);
+  }
 }
 
 module.exports = new PostController();
