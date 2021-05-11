54b3cc584e24127cc0c69db990081ad39bc85d43
edelmutic <edelmutic@gmail.com>
2021-05-11 11:43:50 +0300

    update sql query

--- database.sql
+++ database.sql
@@ -7,8 +7,8 @@ surname VARCHAR(255)
 create TABLE post(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    content VARCHAR(255)
-    user_id INTEGER
+    content VARCHAR(255),
+    user_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES person (id)
-)
+);
 
