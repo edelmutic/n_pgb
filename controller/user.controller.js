5d132117d2606cd70cfc1c02397208ef00cb4788
edelmutic <edelmutic@gmail.com>
2021-05-11 13:31:11 +0300

    update controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -13,7 +13,11 @@ class UserController {
     const users = await db.query('SELECT * FROM person');
     res.json(users.rows);
   }
-  async getOneUser(req, res) {}
+  async getOneUser(req, res) {
+    const id = req.params.id;
+    const user = await db.query('SELECT * FROM person where id = $1', [id]);
+    res.json(user.rows[0]);
+  }
   async updateUser(req, res) {}
   async deleteUser(req, res) {}
 }
